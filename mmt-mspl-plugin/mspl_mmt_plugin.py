09bcee82ac7ea9f3e9d7d1d285c6af0aebf0cd28
mmt-tools <gustavo.jodar@gmail.com>
2024-06-06 13:49:46 +0000

    update pingofdeath input

--- mmt-mspl-plugin/mspl_mmt_plugin.py
+++ mmt-mspl-plugin/mspl_mmt_plugin.py
@@ -15,42 +15,43 @@ NEW_RULE_NUMBER = "114"
 
 #Class that reads the .xml file from the MSPL and gets the relavant data for mmt-security
 class Mspl:
- 
     def parsepacketfilter(self, config_rule, namespace=None):
-        if namespace == None:
+        if namespace is None:
             namespace = 'http://modeliosoft/xsddesigner/a22bd60b-ee3d-425c-8618-beb6a854051a/ITResource.xsd'
+        
         rules = []
-        #packetfiltercondition   
+        # packetfiltercondition
         packet_filter_condition = config_rule.find(".//{%s}packetFilterCondition" % namespace)
         if packet_filter_condition is not None:
             src_address = packet_filter_condition.find(".//{%s}SourceAddress" % namespace)
             dst_address = packet_filter_condition.find(".//{%s}DestinationAddress" % namespace)
             dst_port = packet_filter_condition.find(".//{%s}DestinationPort" % namespace)
             protocol_type = packet_filter_condition.find(".//{%s}ProtocolType" % namespace)
-            payload = packet_filter_condition.find(".//{%s}Payload" % namespace)
-            size = packet_filter_condition.find(".//{%s}Size" % namespace)
+            packet_size = packet_filter_condition.find(".//{%s}packetSize" % namespace)
+            size_operator = packet_size.find(".//{%s}operator" % namespace) if packet_size is not None else None
+            size_bytes = packet_size.find(".//{%s}bytes" % namespace) if packet_size is not None else None
             direction = packet_filter_condition.find(".//{%s}direction" % namespace)
+            icmp_condition = packet_filter_condition.find(".//{%s}ICMPCondition" % namespace)
             tcp_condition = packet_filter_condition.find(".//{%s}TCPCondition" % namespace)
             syn_flag = None
             if tcp_condition is not None:
                 syn_flag = tcp_condition.find(".//{%s}SYNFlag" % namespace)
 
-
-        rule_content_aux = {"src_address": src_address.text if src_address is not None else None,
-                        "dst_address": dst_address.text if dst_address is not None else None,
-                        "dst_port": dst_port.text if dst_port is not None else None,
-                        "protocol_type": protocol_type.text if protocol_type is not None else None,
-                        "payload": payload.text if payload is not None else None,
-                        "size": size.text if size is not None else None,
-                        "direction": direction.text if direction is not None else None,
-                        "syn_flag": syn_flag.text if syn_flag is not None else None
-                        }
-        rule_content = {}
-        for key in rule_content_aux:
-            if(rule_content_aux[key] != None):
-                rule_content[key] = rule_content_aux[key] 
+        rule_content_aux = {
+            "src_address": src_address.text if src_address is not None else None,
+            "dst_address": dst_address.text if dst_address is not None else None,
+            "dst_port": dst_port.text if dst_port is not None else None,
+            "protocol_type": protocol_type.text if protocol_type is not None else None,
+            "size_operator": size_operator.text if size_operator is not None else None,
+            "size_bytes": size_bytes.text if size_bytes is not None else None,
+            "direction": direction.text if direction is not None else None,
+            "syn_flag": syn_flag.text if syn_flag is not None else None,
+            "icmp_type": icmp_condition.find(".//{%s}Type" % namespace).text if icmp_condition is not None else None
+        }
+        
+        rule_content = {key: value for key, value in rule_content_aux.items() if value is not None}
         return rule_content
-    
+
     def is_monitoring(self, configurationRule, namespace=None):
         if namespace == None:
             namespace = 'http://modeliosoft/xsddesigner/a22bd60b-ee3d-425c-8618-beb6a854051a/ITResource.xsd'
@@ -107,21 +108,20 @@ class ConfigAdapter:
         print("  -> New mmt-probe.conf file created...")
 
 class RuleMaker:
-
     def create_rule(self, rules):
-
         for i, rule in enumerate(rules):
             packet_filter_condition = rule["packetFilterCondition"]
-            #about ping_of_the_death
-            if packet_filter_condition["protocol_type"] == '1' and packet_filter_condition["size"][0] == '>':
-
-                changes = ["2" , f"((ip.src != ip.dst) && (meta.packet_len >  {rules[0]['packetFilterCondition']['size'][1:]}))"]
-                                
+            
+            # Handling Ping of Death
+            if packet_filter_condition["protocol_type"] == '1' and packet_filter_condition.get("size_operator") == 'greater':
+                size = packet_filter_condition["size_bytes"]
+                changes = ["2", f"((ip.src != ip.dst) && (meta.packet_len > {size}))"]
+                
                 # Loading rule
                 rule_source_object = ET.parse("rules/51.ping_of_death.xml")
                 root = rule_source_object.getroot()
                 property_element = root.find(".//property")
-                property_element.set("description", "Ping of death attack - size > " + rules[0]['packetFilterCondition']['size'][1:])
+                property_element.set("description", "Ping of death attack - size > " + size)
 
                 # Finding the event element with the specified ID
                 event_element = property_element.find(f"./event[@event_id='{changes[0]}']")
@@ -132,15 +132,13 @@ class RuleMaker:
                 # Write the modified XML as a new rule
                 rule_source_object.write("rules/new_rule.xml")
             
+            # Handling TCP SYN Scan
             #about nmap snyc 
             elif packet_filter_condition["protocol_type"].lower() == 'tcp' and packet_filter_condition.get("syn_flag", "").lower() == 'true':
                 # Loading rule
                 rule_source_object = ET.parse("rules/40.TCP_SYN_scan.xml")
                 # Write the modified XML as a new rule
                 rule_source_object.write("rules/new_rule.xml")
-
-            
-                
 
             
 if __name__ == "__main__":
